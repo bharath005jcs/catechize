483e15209c87ad0425ca813335913b26bb1db92d
bharath005jcs <bharath005jcs@gmail.com>
2015-10-24 08:00:04 +0530

    Catechize - RhoMobile Application

---
+++ public/js/subject_id.js
@@ -0,0 +1,23 @@
+function getId(y)
+{
+	if(y=="Management")
+	{
+		sessionStorage.id = 71;
+	}
+	else if(y=="Wireless")
+	{
+		sessionStorage.id = 72;
+  	}
+  	else if(y=="Forensics")
+  	{
+  		sessionStorage.id = 73;
+  	}
+  	else if(y=="Big Data")
+  	{
+  		sessionStorage.id = 74;
+  	}
+  	else
+  	{
+  		sessionStorage.id = 75;
+  	}
+}
